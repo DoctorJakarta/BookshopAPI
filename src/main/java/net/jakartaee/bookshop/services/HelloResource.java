b5d9fed56008a4dbd7a1037983e8e20da8cf9298
DoctorJava <scott.forbes.in@gmail.com>
2018-12-29 16:48:47 -0600

    Intitial commit.

---
+++ src/main/java/net/jakartaee/bookshop/services/HelloResource.java
@@ -0,0 +1,19 @@
+package net.jakartaee.bookshop.services;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("hello")
+public class HelloResource {
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public Response getText() {
+        return Response.ok("Hello", MediaType.TEXT_HTML).build();
+    }
+
+    
+}
